e8cddaf1bbc512c88663709c822fb8303bb48a3e
nhn-academy-marco <marco@ nhnacademy.com>
2024-08-05 17:31:43 +0900

    Thread 상태 추가

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -22,11 +22,13 @@ public class App
     {
         CounterHandler counterHandler = new CounterHandler(10l);
         Thread thread = new Thread(counterHandler);
+        log.debug("thread-state:{}",thread.getState());
         thread.setName("my-counter");
         thread.start();
         //TODO#1 thread가 실행 후 (1-10 count 증가 후  아래 로그가 출력 됩니다.)
         //thread.join()을 호출 하면 thread가 종료될 때 까지 main thread가 대기하게 됩니다.
 
         log.debug("Application exit!");
+        log.debug("thread-state:{}",thread.getState());
     }
 }
