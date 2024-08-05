2153cf3411030bdae2629327f2316538afceca36
nhn-academy-marco <marco@ nhnacademy.com>
2024-08-05 15:13:35 +0900

    counter-thread 예제 수정

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -19,22 +19,21 @@ public class App
     public static void main( String[] args )
     {
         //TODO#1 CounterHandler 객체를 생성 합니다. countMaxSize : 10
-        CounterHandler counterHandler = new CounterHandler(10l);
+        CounterHandler counterHandler;
 
         //TODO#2 threadA 생성시 counterHandler 객체를 paramter로 전달 합니다.
-        Thread threadA = new Thread(counterHandler);
-        //TODO#3 threadA의 name을 my-counter로 설정 합니다.
-        threadA.setName("my-counter-A");
+        Thread threadA;
+
+        //TODO#3 threadA의 name을 'my-counter-A' 로 설정 합니다.
 
         //TODO#4 threadB 생성시 counterHandler 객체를 paramter로 전달 합니다.
-        Thread threadB = new Thread(counterHandler);
-        //TODO#5 threadB의 name을 my-counter로 설정 합니다.
-        threadB.setName("my-counter-B");
+        Thread threadB;
+
+        //TODO#5 threadB의 name을 'my-counter-B' 로 설정 합니다.
 
         //TODO#7 threadA를 시작 합니다.
-        threadA.start();
 
         //TODO#8 threadB를 시작 합니다.
-        threadB.start();
+
     }
 }
