8daf90bedc1041c4d2db3f9968e794f944e64b8f
nhn-academy-marco <marco@ nhnacademy.com>
2024-08-10 11:45:45 +0900

    yield 예제 추가

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -20,43 +20,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        //TODO#1 counterHandlerA 객체를 생성 합니다. countMaxSize : 10
+        //counterHandlerA 객체를 생성 합니다. countMaxSize : 10
         CounterHandler counterHandlerA = new CounterHandler(10l);
-        //TODO#2 threadA 생성시 counterHandlerA 객체를 paramter로 전달 합니다.
+        //threadA 생성시 counterHandlerA 객체를 paramter로 전달 합니다.
         Thread threadA = new Thread(counterHandlerA);
-        //TODO#3 threadA의 name을 'my-counter-A' 로 설정 합니다.
+        //threadA의 name을 'my-counter-A' 로 설정 합니다.
         threadA.setName("my-counter-A");
         log.debug("threadA-state:{}",threadA.getState());
 
-        //TODO#4 counterHandlerB 객체를 생성 합니다. countMaxSize : 10
+        //counterHandlerB 객체를 생성 합니다. countMaxSize : 10
         CounterHandler counterHandlerB = new CounterHandler(10l);
-        //TODO#5 threadB 생성시 counterHandlerB 객체를 paramter로 전달 합니다.
+        //threadB 생성시 counterHandlerB 객체를 paramter로 전달 합니다.
         Thread threadB = new Thread(counterHandlerB);
-        //TODO#6 threadB의 name을 'my-counter-B' 로 설정 합니다.
+        //threadB의 name을 'my-counter-B' 로 설정 합니다.
         threadB.setName("my-counter-B");
         log.debug("threadB-state:{}",threadB.getState());
 
-        //TODO#7 threadA를 시작 합니다.
+        //threadA를 시작 합니다.
         threadA.start();
-        try {
-            //TODO#8 threadA 작업이 완료될 때까지 main Thread는 대기 합니다.
-            threadA.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         log.debug("threadA-state:{}",threadA.getState());
 
-        //TODO#9 threadB를 시작 합니다.
+        //threadB를 시작 합니다.
         threadB.start();
-        try {
-            //TODO#10 threadB 작업이 완료될 때까지 main Thread는 대기 합니다.
-            threadB.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         log.debug("threadB-state:{}",threadB.getState());
 
-        //TODO#11 'Application exit!' message를 출력 합니다.
+        //TODO#1 Main Thread가 threadA, ThreadB가 종료될 때 까지 대기 합니다. Thread.yield를 사용 합니다.
+        do {
+            Thread.yield();
+        }while (threadA.isAlive() || threadB.isAlive() );
+        //'Application exit!' message를 출력 합니다.
         log.debug("Application exit!");
+
     }
 }
