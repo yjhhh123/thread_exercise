991cee14f74d209b52d92b32a79a184e38abe45a
yunjiho <yoonwlgh12@gmail.com>
2024-09-10 14:38:30 +0900

    step4

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -20,20 +20,18 @@ public class App
     {
         //TODO#1 CounterHandler 객체를 생성 합니다. countMaxSize : 10
         CounterHandler counterHandler;
-
+        counterHandler = new CounterHandler(10);
         //TODO#2 threadA 생성시 counterHandler 객체를 paramter로 전달 합니다.
-        Thread threadA;
-
+        Thread threadA = new Thread(counterHandler);
         //TODO#3 threadA의 name을 'my-counter-A' 로 설정 합니다.
-
+        threadA.setName("my-thread-A");
         //TODO#4 threadB 생성시 counterHandler 객체를 paramter로 전달 합니다.
-        Thread threadB;
-
+        Thread threadB = new Thread(counterHandler);
         //TODO#5 threadB의 name을 'my-counter-B' 로 설정 합니다.
-
+        threadB.setName("my-thread-B");
         //TODO#7 threadA를 시작 합니다.
-
+        threadA.start();
         //TODO#8 threadB를 시작 합니다.
-
+        threadB.start();
     }
 }
