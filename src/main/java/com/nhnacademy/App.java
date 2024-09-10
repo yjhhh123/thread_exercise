4be06a7569ab89bb0d5e2442f3284837b352b7ec
yunjiho <yoonwlgh12@gmail.com>
2024-09-10 15:03:16 +0900

    step7

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -45,7 +45,19 @@ public class App
         log.debug("threadB-state:{}",threadB.getState());
 
         //TODO#1 Main Thread가 threadA, ThreadB가 종료될 때 까지 대기 합니다. Thread.yield를 사용 합니다.
-        
+        Thread.yield();
+        try{
+            threadA.join();
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try{
+            threadB.join();
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         // threadA, threadB가 종료되면 'Application exit!' message를 출력 합니다.
         log.debug("Application exit!");
 
